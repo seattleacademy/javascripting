ed6ae58987781cd4c6a849d3c374f1db81cf5cb2
Gary Anderson <ganderson@seattleacademy.org>
2016-01-31 11:23:49 -0800

    looping-through-arrays.js

---
+++ looping-through-arrays.js
@@ -0,0 +1,29 @@
+/*
+ ## The challenge:
+
+  Create a file named looping-through-arrays.js.
+
+  In that file, define a variable named pets that references this array:
+
+     ['cat', 'dog', 'rat'];
+
+  Create a for loop that changes each string in the array so that they are
+  plural.
+
+  You will use a statement like this inside the for loop:
+
+     pets[i] = pets[i] + 's';
+
+  After the for loop, use console.log() to print the pets array to the
+  terminal.
+
+  Check to see if your program is correct by running this command:
+
+     javascripting verify looping-through-arrays.js
+*/
+
+pets = ['cat', 'dog', 'rat'];
+for(i=0; i<pets.length; i++){
+  pets[i] = pets[i] + 's';
+}
+console.log(pets)
