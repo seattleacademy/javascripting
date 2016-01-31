010c573bc86d4ad6471ed883a99ad4c67a6380d6
Gary Anderson <ganderson@seattleacademy.org>
2016-01-31 10:37:43 -0800

    add number-to-string.js

---
+++ number-to-string.js
@@ -0,0 +1,19 @@
+ 
+ /*## The challenge:
+
+  Create a file named number-to-string.js.
+
+  In that file define a variable named n that references the number 128;
+
+  Call the .toString() method on the n variable.
+
+  Use console.log() to print the results of the .toString() method to the
+  terminal.
+
+  Check to see if your program is correct by running this command:
+
+     javascripting verify number-to-string.js
+*/
+
+example = 128;
+console.log(example.toString());
