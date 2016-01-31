f65afc0c56b0eddd25296a1e08a6a53b62a9cada
Gary Anderson <ganderson@seattleacademy.org>
2016-01-31 11:17:01 -0800

    add accessing-array-values.js

---
+++ accessing-array-values.js
@@ -0,0 +1,18 @@
+/*
+ ## The challenge:
+
+  Create a file named accessing-array-values.js.
+
+  In that file, define array food :
+
+     var food = ['apple', 'pizza', 'pear'];
+
+  Use console.log() to print the second value of array to the terminal.
+
+  Check to see if your program is correct by running this command:
+
+     javascripting verify accessing-array-values.js
+*/
+
+var food = ['apple', 'pizza', 'pear'];
+console.log(food[1]);
