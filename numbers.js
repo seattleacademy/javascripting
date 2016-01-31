3cf221f96c64305cdedaec56dea377ace702c220
Gary Anderson <ganderson@seattleacademy.org>
2016-01-31 10:36:54 -0800

    add numbers

---
+++ numbers.js
@@ -0,0 +1,17 @@
+/*
+ ## The challenge:
+
+  Create a file named numbers.js.
+
+  In that file define a variable named example that references the integer
+  123456789.
+
+  Use console.log() to print that number to the terminal.
+
+  Check to see if your program is correct by running this command:
+
+  javascripting verify numbers.js
+*/
+
+var example = 123456789;
+console.log(example);
