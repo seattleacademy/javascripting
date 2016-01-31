ad37fd070d59f8bee03a14a8359b835be035e6de
Gary Anderson <ganderson@seattleacademy.org>
2016-01-31 10:19:25 -0800

    add revising-strings.js

---
+++ revising-strings.js
@@ -0,0 +1,19 @@
+ // ## The challenge:
+
+ //  Create a file named revising-strings.js.
+
+ //  Define a variable named pizza that references this string: 'pizza is
+ //  alright'
+
+ //  Use the .replace() method to change alright to wonderful.
+
+ //  Use console.log() to print the results of the .replace() method to the
+ //  terminal.
+
+ //  Check to see if your program is correct by running this command:
+
+ //  javascripting verify revising-strings.js
+
+var pizza = 'pizza is alright';
+pizza = pizza.replace('alright','wonderful');
+console.log(pizza);
