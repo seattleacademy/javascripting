4e840a2c64b235004c0cc13f51cba7775246ff49
Gary Anderson <ganderson@seattleacademy.org>
2016-01-31 10:54:44 -0800

    add for-loop.js

---
+++ for-loop.js
@@ -0,0 +1,31 @@
+/*
+ ## The challenge:
+
+  Create a file named for-loop.js.
+
+  In that file define a variable named total and make it equal the number 0.
+
+  Define a second variable named limit and make it equal the number 10.
+
+  Create a for loop with a variable i starting at 0 and increasing by 1 each
+  time through the loop. The loop should run as long as i is less than
+  limit.
+
+  On each iteration of the loop, add the number i to the total variable. To
+  do this, you can use this statement:
+
+     total += i;
+
+  After the for loop, use console.log() to print the total variable to the
+  terminal.
+
+  Check to see if your program is correct by running this command:
+
+     javascripting verify for-loop.js
+*/
+var total = 0;
+var limit = 10;
+for(i=0; i<10; i++){
+  total += i;
+}
+console.log(total);
